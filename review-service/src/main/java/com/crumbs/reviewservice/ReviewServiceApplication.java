972eea3593559b39d3c45ca4a2b46fee2aacd67d
lmehmedagi1 <lmehmedagi1@etf.unsa.ba>
2021-03-30 18:03:44 +0200

    Eureka service discovery setup

--- review-service/src/main/java/com/crumbs/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/com/crumbs/reviewservice/ReviewServiceApplication.java
@@ -2,11 +2,22 @@ package com.crumbs.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReviewServiceApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
