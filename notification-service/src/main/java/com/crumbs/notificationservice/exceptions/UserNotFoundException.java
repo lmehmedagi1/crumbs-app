cdac693bc505cc48e79e443e45040dc2f4c7405c
Arslan <aturkusic1@etf.unsa.ba>
2021-04-08 21:55:08 +0200

    Comm between notification and user services, tests

--- notification-service/src/main/java/com/crumbs/notificationservice/exceptions/UserNotFoundException.java
+++ notification-service/src/main/java/com/crumbs/notificationservice/exceptions/UserNotFoundException.java
@@ -1,4 +1,24 @@
 package com.crumbs.notificationservice.exceptions;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+
+    private String altMessage;
+
+    public UserNotFoundException() {
+        super("User not found");
+    }
+
+    public UserNotFoundException(String altMessage) {
+        this();
+        this.altMessage = altMessage;
+    }
+
+    public String getAltMessage() {
+        return altMessage;
+    }
+
+    public void setAltMessage(String altMessage) {
+        this.altMessage = altMessage;
+    }
+
 }
