8f55dd949dac545a78301fd1d9defde02dfcda77
mpaldum1 <mpaldum1@etf.unsa.ba>
2021-04-14 23:35:04 +0200

    System events initialize

---
+++ system-events/src/main/java/com/crumbs/systemevents/SystemEventsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crumbs.systemevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemEventsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemEventsServiceApplication.class, args);
+	}
+
+}
