c41a0de4df7a46f7f8a8246c1322e1754abd2ea2
Arslan <aturkusic1@etf.unsa.ba>
2021-05-17 00:04:06 +0200

    Home page frontend

--- frontend/src/components/common/recipeCard.js
+++ frontend/src/components/common/recipeCard.js
@@ -1,31 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
 
 function RecipeCard(props) {
     return (
         <div className="itemCard">
-            <Link to={props.recipe.url}>
-                <div className="itemImage">
-                    <img src={props.recipe.image} alt="Product image"/>
-                </div>
-                <div className="itemInfo">
-                <div className="itemName">
-                    {props.recipe.name}
-                </div>
-                <div className="itemDetails">
-                    {props.recipe.details}
-                </div>
-                <div className="itemPrice">
-                    Start from ${props.recipe.startingPrice}
-                </div>
-                <div className="itemButtons">
-                    <Button variant="primary" type="submit">
-                        Bid <i className="fa fa-gavel" aria-hidden="true"></i>
-                    </Button>
-                </div>
-                </div>
-            </Link>
+            <Card  border="primary" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://source.unsplash.com/random/300x300" />
+                <Card.Body>
+                <Card.Title>{props.ButtonrecepieName}</Card.Title>
+                <Card.Text>
+                   {props.text}
+                </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                <small className="text-muted" style={{float: 'right'}} >{props.author}</small>
+                </Card.Footer>
+            </Card>
         </div>
     )
 }
--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom'
+import { Row, Col } from 'react-bootstrap'
 import Menu from 'components/common/menu'
+import RecipeCard from 'components/common/recipeCard'
+import 'components/home/home.scss'
+import Button from 'react-bootstrap/Button'
 
 function Home(props) {
+    const [counter, setCounter] = useState(0);
+    const [counterMP, setCounterMP] = useState(0);
+    const [products, setProducts] = useState([
+        {
+            recepieName: "Ime recepta",
+            text: "neki text bla bla bla bala 1",
+            author: "neki autor 1",
+        },
+        {
+            recepieName: "Ime recepta2",
+            text: "neki text bla bla bla bala 2",
+            author: "neki autor 2",
+        },
+        {
+            recepieName: "Ime recepta3",
+            text: "neki text bla bla bla bala3",
+            author: "neki autor 3",
+        }
+
+    ]);
+
+    const [productsMP, setProductsMP] = useState([
+        {
+            recepieName: "Ime recepta",
+            text: "neki text bla bla bla bala 1",
+            author: "neki autor 1",
+        },
+        {
+            recepieName: "Ime recepta2",
+            text: "neki text bla bla bla bala 2",
+            author: "neki autor 2",
+        },
+        {
+            recepieName: "Ime recepta3",
+            text: "neki text bla bla bla bala3",
+            author: "neki autor 3",
+        },
+        {
+            recepieName: "Ime recepta4",
+            text: "neki text bla bla bla bala3",
+            author: "neki autor 4",
+        }
+
+    ]);
 
     const handleSearchChange = search => {
         props.history.push({
@@ -13,8 +61,29 @@ function Home(props) {
 
     return (
         <div>
+            
             <Menu handleSearchChange={handleSearchChange} {...props}/>
-            <p>Hoem page</p>
+            <p>Home page</p>
+            <Row>
+                <h2 className="titleHome">Daily recepies</h2>
+            </Row>
+            <Row id="dailyGrid" >
+                {products.map((product, index) => (
+                        <RecipeCard {...product}> </RecipeCard>
+                            
+                        ))}
+                <Button style={{height: "30px", width: "70px", alignSelf: "center"}}>Next</Button>
+            </Row>
+            <Row >
+                <h2 className="titleHome">Most popular recepies</h2>
+            </Row>
+            <Row id="mostPopularGrid" >
+                {productsMP.map((product, index) => (
+                         <RecipeCard {...product}> </RecipeCard>
+                            
+                        ))}
+                <Button style={{height: "30px", width: "70px", alignSelf: "center"}}>Next</Button>
+            </Row>
         </div>
     )
 }
