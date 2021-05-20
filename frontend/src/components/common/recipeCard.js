bc324f1069cc344ac399644e229cfd10e245ed0c
Arslan <arslanaki96@gmail.com>
2021-05-20 23:42:26 +0200

    Frontend changes, most popular recipes retreive

--- frontend/src/components/common/recipeCard.js
+++ frontend/src/components/common/recipeCard.js
@@ -7,13 +7,13 @@ function RecipeCard(props) {
             <Card border="primary" style={{ width: '18rem', color: "black" }}>
                 <Card.Img variant="top" src="https://source.unsplash.com/random/300x300" />
                 <Card.Body>
-                    <Card.Title>{props.ButtonrecipeName}</Card.Title>
+                    <Card.Title>{props.title}</Card.Title>
                     <Card.Text style={{ textDecoration: "none" }}>
-                        {props.text}
+                        {props.description}
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted" style={{ float: 'right' }} >{props.author}</small>
+                    <small className="text-muted" style={{ float: 'right' }} >{props.author.username}</small>
                 </Card.Footer>
             </Card>
         </div >
--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,16 +1,42 @@
 import Menu from 'components/common/menu'
 import RecipeCard from 'components/common/recipeCard'
 import 'components/home/home.scss'
+import axios from "axios";
 import RecipeForm from 'components/recipe/recipeForm'
-import React, { useState } from 'react'
+import React, { useState, useEffect  } from 'react'
 import { CardGroup, Container, Row } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import { BsPlusCircleFill } from "react-icons/bs"
 import { ImArrowRight } from "react-icons/im"
 import { Link, withRouter } from 'react-router-dom'
+import { getMostPopularRecipes } from 'actions/recipeActions'
+import { useSelector, useDispatch } from 'react-redux'
+
 
 function Home(props) {
     const [show, setShow] = useState(false);
+    const [count, setCount] = useState(0)
+    const mostPopularRecepies = useSelector(state => state.recipes.mostPopularRecipes);
+    const dispatch = useDispatch()
+    useEffect( () => {
+        // axios.get("http://localhost:8090/recipe-service/recipes/topMonthly", {
+        //     params: {
+        //         pageNo: count
+        //     },
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3NWE4ZjM0Yi0yNTM5LTQ1MmEtOTMyNS1iNDMyZGJlM2I5OTUiLCJpYXQiOjE2MjE1MzU2NjgsImV4cCI6MTYyMTYzNDQwMH0.L57oPUx_l_a8gzlOIJVU4hYK7YZSA-VUevwv_zvOWXtlC92mlFkf6f6rWlTzOorhSelfWCmR2eg1ZOq2M4RImQ'
+        //     }
+        // }
+        // ).then((response) => {
+        //     console.log("idemoo", response.data._embedded.recipeViewList);
+        //     setProductsMP(response.data._embedded.recipeViewList)
+        //   })
+        //   .catch((error) => console.log(error));
+        dispatch(getMostPopularRecipes(count))
+      
+     }, []);
+
     const [products] = useState([
         {
             recipeName: "Ime recepta",
@@ -29,33 +55,6 @@ function Home(props) {
             text: "Learn to swim 3",
             author: "Autor 3",
             id: 3
-        }
-    ]);
-
-    const [popularProducts, setProductsMP] = useState([
-        {
-            recipeName: "Ime recepta",
-            text: "Learn to swim 1",
-            author: "Autor 1",
-            id: 1
-        },
-        {
-            recipeName: "Ime recepta 2",
-            text: "Learn to swim 2",
-            author: "Autor 2",
-            id: 2
-        },
-        {
-            recipeName: "Ime recepta 3",
-            text: "Learn to swim 3",
-            author: "Autor 3",
-            id: 3
-        },
-        {
-            recipeName: "Ime recepta 4",
-            text: "Learn to swim 3",
-            author: "Autor 4",
-            id: 4
         }
     ]);
 
@@ -78,7 +77,7 @@ function Home(props) {
 
             <CardGroup id="dailyGrid">
                 {products.map(product => (
-                    <Link to={"recipe/" + product.id}>
+                    <Link to={"recipe/" + product.recipeId}>
                         <RecipeCard {...product}> </RecipeCard>
                     </Link>
 
@@ -90,9 +89,10 @@ function Home(props) {
 
             <Row className="headerText">Most Popular Recipes</Row>
             <Row id="mostPopularGrid" >
-                {popularProducts.map(product => (
-                    <Link to={"recipe/" + product.id}>
-                        <RecipeCard {...product}> </RecipeCard>
+                {console.log("arslan", mostPopularRecepies)}
+                {mostPopularRecepies.map(recipe => (
+                    <Link to={"recipe/" + recipe.recipeId}>
+                        <RecipeCard {...recipe}> </RecipeCard>
                     </Link>))}
 
                 <Button variant="outline-primary" className="nextBtn" >
