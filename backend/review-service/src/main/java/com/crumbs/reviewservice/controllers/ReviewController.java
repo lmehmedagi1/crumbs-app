772d59c22d599f185ed79cf27fac3052495a1049
lmehmedagi1 <lmehmedagi1@etf.unsa.ba>
2021-05-20 23:57:48 +0200

    Uncommenting arslans comments

--- backend/review-service/src/main/java/com/crumbs/reviewservice/controllers/ReviewController.java
+++ backend/review-service/src/main/java/com/crumbs/reviewservice/controllers/ReviewController.java
@@ -10,7 +10,7 @@ import com.crumbs.reviewservice.utility.JwtConfigAndUtil;
 import com.crumbs.reviewservice.utility.assemblers.ReviewModelAssembler;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -44,15 +44,15 @@ public class ReviewController {
     private final ReviewService reviewService;
     private final ReviewModelAssembler reviewModelAssembler;
     private final ReviewWebClientRequest reviewWebClientRequest;
-    //private final RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
 
     @Autowired
     ReviewController(ReviewService reviewService, ReviewModelAssembler reviewModelAssembler,
-                     ReviewWebClientRequest reviewWebClientRequest /*, RabbitTemplate rabbitTemplate*/) {
+                     ReviewWebClientRequest reviewWebClientRequest , RabbitTemplate rabbitTemplate) {
         this.reviewService = reviewService;
         this.reviewModelAssembler = reviewModelAssembler;
         this.reviewWebClientRequest = reviewWebClientRequest;
-        //this.rabbitTemplate = rabbitTemplate;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @RequestMapping(params = "id", method = RequestMethod.GET)
@@ -108,8 +108,8 @@ public class ReviewController {
         final Review newReview = reviewService.createReview(reviewRequest, userId);
         EntityModel<Review> entityModel = reviewModelAssembler.toModel(newReview);
 
-//        ReviewCreatedEvent createdEvent = new ReviewCreatedEvent(UUID.randomUUID().toString(), newReview.getId(), reviewRequest.getComment());
-//        rabbitTemplate.convertAndSend("REVIEW_EXCHANGE", "REVIEW_ROUTING_KEY", createdEvent);
+        ReviewCreatedEvent createdEvent = new ReviewCreatedEvent(UUID.randomUUID().toString(), newReview.getId(), reviewRequest.getComment());
+        rabbitTemplate.convertAndSend("REVIEW_EXCHANGE", "REVIEW_ROUTING_KEY", createdEvent);
 
         return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
