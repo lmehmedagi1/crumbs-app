f7e706dcd64965afeaf9190707439a4fa5c235a0
lmehmedagi1 <lmehmedagi1@etf.unsa.ba>
2021-06-04 02:08:13 +0200

    Recipe delete

--- frontend/src/components/router/router.js
+++ frontend/src/components/router/router.js
@@ -1,20 +1,20 @@
+import Browse from 'components/browse/browse';
+import NotFound from 'components/common/notFound';
+import BrowseDiets from 'components/diet/browseDiets';
+import Diet from 'components/diet/diet';
+import Home from 'components/home/home';
+import Login from 'components/login/login';
+import People from 'components/people/people';
+import Profile from 'components/profile/profile';
+import RecipePreview from 'components/recipe/recipePreview';
+import Register from 'components/register/register';
+import PasswordReset from 'components/resetPassword/resetPassword';
+import ScrollToTop from 'components/utility/scrollToTop';
 import React from 'react';
 import {
   Route, Switch
 } from "react-router-dom";
-import ScrollToTop from 'components/utility/scrollToTop'
 
-import NotFound from 'components/common/notFound'
-import Login from 'components/login/login'
-import Register from 'components/register/register'
-import Home from 'components/home/home'
-import Browse from 'components/browse/browse'
-import BrowseDiets from 'components/diet/browseDiets'
-import Diet from 'components/diet/diet'
-import People from 'components/people/people'
-import Profile from 'components/profile/profile'
-import RecipePreview from 'components/recipe/recipePreview'
-import PasswordReset from 'components/resetPassword/resetPassword'
 
 function Routes(props) {
   return (
